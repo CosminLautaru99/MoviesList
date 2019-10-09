b0501c29d27ed3e7004bb361886aa4c0ba1ea1be
CosminLautaru99 <50625287+CosminLautaru99@users.noreply.github.com>
2019-10-09 22:45:30 +0300

    Movie List

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
-const header = document.querySelector(".header")
+const header = document.querySelector(".header-banner")
 const movies_card = document.querySelector("#movie-card")
 const movies_name = document.querySelector("#name")
 const movies_search_button = document.querySelector(".search-button")
 const search_box_form = document.querySelector("#search-box-form")
+const info_alert = document.querySelector(".info-alert")
+const alert_error = document.querySelector(".alert-error")
 let movies_rating
 let movies_data = []
 let movies_rating_form
@@ -10,10 +12,11 @@ let movies_rating_button
 let movies_delete_button
 let card_body
 let selected_movies = []
-let selected_movies_names = []
+let selected_movies_id = []
 let movies_rating_p
 let i
 let j
+
 /* http://www.omdbapi.com/?t=Never+Back+Down+2&plot=full&apikey=5dff865c%20*/
 
 /* Apelare API */
@@ -35,7 +38,6 @@ function searchMovies() {
 /* Render Movies */
 function renderMovies(movies) {
 	selected_movies.push(movies)
-
 	movies_card.insertAdjacentHTML(
 		"afterbegin",
 		`
@@ -69,9 +71,10 @@ function renderMovies(movies) {
 	movies_rating_button.addEventListener("click", () => submitRating())
 }
 
+/* Info */
+
 /* Disable Enable/button de search */
 function onChange_Search() {
-	console.log(movies_name)
 	const movies_search_name = movies_name.value
 	if (movies_search_name) {
 		movies_search_button.removeAttribute("disabled")
@@ -101,11 +104,12 @@ function submitRating() {
 		<p class="rate" style="color:white; margin: 0 auto;">Rating: ${rating}</p>
 	`
 	)
-	movies_rating_form.classList.add("is-hidden")
+	movies_rating_form.classList.add("d-none")
 	movies_rating_button.card - card_body.removeChild(movies_rating_button)
 	movies_delete_button.card - card_body.removeChild(movies_delete_button)
 	movies_search_button.removeAttribute("disabled")
-	search_box_form.classList.remove("is-hidden")
+	search_box_form.classList.remove("d-none")
+	info_alert.classList.add("d-none")
 }
 
 /* Functia de cautare filme */
@@ -117,19 +121,39 @@ function onSearch() {
 		movies_rating.addEventListener("input", () => onChange())
 		movies_search_button.setAttribute("disabled", true)
 		movies_name.value = ""
-		search_box_form.classList.add("is-hidden")
-		/* selected_movies_names.push(movies_name.value)
-		for (i = 0; i > selected_movies_names.length; i++) {
-			if (movies_name.value == selected_movies_names[i]) {
-				console.log("123")
-			} 
-		} */
+		search_box_form.classList.add("d-none")
+		info_alert.classList.remove("d-none")
+		selected_movies_id.push(movies_data.imdbID)
+		for (i = 0; i < selected_movies_id.length; i++) {
+			for (j = 1; j < selected_movies_id.length + 1; j++) {
+				if (i == j) {
+					j++
+				}
+				if (selected_movies_id[i] == selected_movies_id[j]) {
+					console.log(`i=${i}, j=${j}`)
+					console.log(
+						`${selected_movies_id[i - 1]}, ${
+							selected_movies_id[j]
+						} `
+					)
+					deleteMovies()
+					info_alert.classList.add("d-none")
+					alert_error.classList.remove("d-none")
+					search_box_form.classList.remove("d-none")
+					setTimeout(function() {
+						alert_error.classList.add("d-none")
+					}, 2000)
+				} else console.log("a functionat")
+			}
+		}
 	})
 }
 
 /* Functie de delete */
 function deleteMovies() {
 	movies_card.removeChild(remove_movies_card)
+	search_box_form.classList.remove("d-none")
+	info_alert.classList.add("d-none")
 }
 
 movies_name.addEventListener("input", () => onChange_Search())
